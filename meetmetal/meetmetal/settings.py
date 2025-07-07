df6618551594ef9f893044a7547def5c740297be
savaliyasagar <168646014+savaliyasagar@users.noreply.github.com>
2025-07-07 21:09:30 +0530

    Update settings.py

--- meetmetal/meetmetal/settings.py
+++ meetmetal/meetmetal/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-$=5rry$*ld3$4n24fxze%lg9-pu&r612sn+ks4wycs5w2(pb(&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
