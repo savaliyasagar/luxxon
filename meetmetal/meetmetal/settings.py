051c64475fc6496008e68843f9e4493da5491b5f
savaliyasagar <168646014+savaliyasagar@users.noreply.github.com>
2025-07-10 21:48:45 +0530

    Update settings.py

--- meetmetal/meetmetal/settings.py
+++ meetmetal/meetmetal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
+    'meetmetal.home',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
