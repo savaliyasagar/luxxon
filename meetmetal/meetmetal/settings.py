0fafadb0cad25a52802b096f22186b34470518d9
Sagar Savaliya <sagarsavaliya1003@gmail.com>
2025-07-11 21:16:28 +0530

    add new database

--- meetmetal/meetmetal/settings.py
+++ meetmetal/meetmetal/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
+from decouple import config 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,10 +80,7 @@ WSGI_APPLICATION = 'meetmetal.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=config("DATABASE_URL"))
 }
 
 
